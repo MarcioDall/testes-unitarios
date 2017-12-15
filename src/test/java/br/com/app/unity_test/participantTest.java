82f96fd321801c550e7d9bb62ea13aab92ad26fa
Marcio <marcio.dallelaste@involves.com.br>
2017-12-15 09:00:26 -0200

    translation of the participant class from portuguese to English

--- src/test/java/br/com/app/unity_test/participantTest.java
+++ src/test/java/br/com/app/unity_test/participantTest.java
@@ -9,35 +9,35 @@ import br.com.app.data.participant;
 public class participantTest {
 
 	@Test
-	public void Valida_setar_nome() {
+	public void Valid_set_name() {
 		participant p1 = new participant();
 		p1.setNome("Marcio");
 		assertEquals("Marcio", p1.getNome());
 	}
 
 	@Test
-	public void Valida_setar_idade() {
+	public void Valid_set_age() {
 		participant p2 = new participant();
 		p2.setIdade(31);
 		assertEquals(31, p2.getIdade());
 	}
 
 	@Test
-	public void Valida_setar_sobrenome() {
+	public void Valid_set_last_name() {
 		participant p3 = new participant();
 		p3.setLastName("Dallelaste");
 		assertEquals("Dallelaste", p3.getLastName());
 	}
 
 	@Test
-	public void Valida_setar_peso() {
+	public void Valid_set_weight() {
 		participant p4 = new participant();
 		p4.setWeight(100);
 		assertEquals(100, p4.getWeight());
 	}
 	
 	@Test
-	public void Valida_setar_altura() {
+	public void Valid_set_height() {
 		participant p5 = new participant();
 		p5.setAltura(186);
 		assertEquals(186, p5.getAltura());
